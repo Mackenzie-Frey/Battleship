375c5e2f8bdb8962e6a563204522e0963dd73660
Mackenzie-Frey <mackenziefrey1@gmail.com>
2018-10-14 15:34:00 -0600

    Integrate: check_for_shot_vs_existing_coordinate, check_for_duplicate_shots, incorrect_coordinate, altered shoot method. Test: test_player_can_take_a_shot, test_for_duplicate_shots, test_it_is_a_shot_that_exists

--- lib/player.rb
+++ lib/player.rb
@@ -1,28 +1,52 @@
 require './lib/board.rb'
 
 class Player
-  attr_reader :board
+  attr_reader :board,
+              :stored_shot_array
+
+  attr_accessor :shot_1
 
   def initialize
     @board = Board.new.display_grid
+    @stored_shot_array = []
   end
 
   def shoot
     puts "Take your shot!"
     @shot_1 = gets.chomp
 
-    # change status of space to H or M
-    # this prints to board
-
+    if check_for_shot_vs_existing_coordinate(@shot_1) && check_for_duplicate_shots
+      @stored_shot_array << @shot_1
+    else
+      incorrect_coordinate
+    end
   end
 
-  def check_for_dupliates
-    # have array of stored shots
-    # << shovel new shot into existing shots array and check for duplicate shots.
-    # reject if duplicate
-
+  def incorrect_coordinate
+    puts "Sorry, at least one of your coordinates is incorrect. Maybe next time you'll get things right."
+    shoot
   end
 
-# test for duplicate shots
+  def check_for_duplicate_shots
+    @stored_shot_array.uniq == @stored_shot_array
+  end
+
+  def check_for_shot_vs_existing_coordinate(coordinate)
+    existing_coordinate_array = ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4"]
+    existing_coordinate_array.include?(coordinate)
+  end
+
 
 end
+
+
+
+ # match stored shot array against ship placement
+
+
+
+
+  #when is game over
+  # change status of space to H or M
+  # this prints to board
+  # While ship? = false, do loop (asking for shots)
--- test/player_test.rb
+++ test/player_test.rb
@@ -13,8 +13,23 @@ class PlayerTest < Minitest::Test
   end
 
   def test_player_can_take_a_shot
+skip
     player = Player.new
-    assert_equal 0, player.shoot
+    assert_equal ["A1"], player.shoot
+  end
+
+  def test_for_duplicate_shots
+skip
+    player = Player.new
+    player.shoot
+    player.shoot
+    assert_equal false, player.check_for_duplicate_shots
+  end
+
+  def test_it_is_a_shot_that_exists
+    player = Player.new
+    assert_equal true, player.check_for_shot_vs_existing_coordinate("A1")
+    assert_equal false, player.check_for_shot_vs_existing_coordinate("F1")
   end
 
 end
